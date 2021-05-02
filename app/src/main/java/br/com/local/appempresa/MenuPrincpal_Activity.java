2fbcebe4a11256685726dcecc049fca8165052ff
[guguss03] <[81369100+guguss03@users.noreply.github.com]>
2021-05-02 20:04:15 -0300

    Initial commit

---
+++ app/src/main/java/br/com/local/appempresa/MenuPrincpal_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.appempresa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuPrincpal_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_princpal_layout);
+    }
+}
